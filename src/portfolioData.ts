19fade06b771bba1ef764cec75a5561d21cafcc5
siennalansky <siennalansky@gmail.com>
2026-03-24 19:57:32 +0100

    live demo update

--- src/portfolioData.ts
+++ src/portfolioData.ts
@@ -1,58 +1,59 @@
 export const education = [
-    {
-      name: "Chas Academy- JavaScript Fullstack Open Source",
-      description:
-        "Fullstackutvecklare Open Source - JavaScript Denna utbildning innefattar följande kurser: - Bootcamp - JavaScript grunder- Ramverk & Komponentbaserad utveckling - Backendutveckling & API-design med säkerhetsfokus - Kvalitetssäkrad utveckling & Tillgänglighet - Projektbaserad produktutveckling - Avancerad fullstackutveckling, test & devops- LIA",
-      year: "2025-2027",
-    },
-    {
-      name: "Värmdö gymnasium- NaturNatur Journalistik och Forskning",
-      description: "",
-      year: "2017-2020",
-    },
-  ];
-  
-  export const work = [
-    {
-      name: "Riddermark bil - Inköpare, Säljare, Administratör",
-      description:
-        "Köpte och sålde personbilar till både privatpersoner och företag. Ansvarade även för administrativa uppgifter som kontraktshantering och betalningar.",
-      year: "2024-2025",
-    },
-    {
-      name: "Vårdcentralen Hökarängen - Vårdbiträde, Receptionist",
-      description:
-        "Skötte administrativt arbete, post, fakturor, bokningar, telefon och registrering av patienter. Utförde hembesök som del av hemsjukvården.",
-      year: "2018-2024",
-    },
-  ];
-  
-  export const projects = [
-    {
-      name: "Spa app",
-      description:
-        "En spa-applikation med integrerad webshop, bokningssystem och varukorg. Byggd för att ge användare en smidig upplevelse från att boka behandlingar till att köpa produkter online.",
-      github: "https://github.com/siensky/glowspa",
-      live: "https://glowspa.vercel.app/",
-    },
-    {
-      name: "Bibliotek backend",
-      description:
-        "Byggde ett backend-API för ett bibliotekssystem med Bun och PostgreSQL, med full CRUD-funktionalitet för böcker och författare. Implementerade autentisering med Auth0, rollbaserad åtkomst och ett lånesystem för att låna och lämna tillbaka böcker.",
-      github: "https://github.com/siensky/library",
-      live: "https://library-production-911d.up.railway.app/",
-    },
-    {
-      name: "Bibliotek i php",
-      description:
-        "Ett övningsprojekt i PHP där jag bygger motsvarande endpoints till ett bibliotekssystem. Fokus ligger på att hantera data från en befintlig databas, inklusive att lista böcker och författare samt implementera funktionalitet för boklån.",
-      github: "https://github.com/yourusername/weather-app",
-      live: "https://weather-app.vercel.app",
-    },
-    {
-      name: "Vinkatalog vanilla JavaScript",
-      description: "Ett responsivt webbprojekt byggt med HTML, CSS och vanilla JavaScript som en del av en bootcamp på Chas Academy. Fokus låg på att fördjupa förståelsen för grunderna i frontendutveckling utan ramverk, med särskild hänsyn till tillgänglighet enligt WCAG och användarvänlig design. Webbplatsen innehåller dynamiskt innehåll hämtat från ett API samt en mobilanpassad navigationsmeny med hamburgerfunktion.",
-      github: "https://https://github.com/siensky/examinationsuppgift",
-      live: "https://wines-bice.vercel.app/index.html",
-    },
-  ];
+  {
+    name: "Chas Academy- JavaScript Fullstack Open Source",
+    description:
+      "Fullstackutvecklare Open Source - JavaScript Denna utbildning innefattar följande kurser: - Bootcamp - JavaScript grunder- Ramverk & Komponentbaserad utveckling - Backendutveckling & API-design med säkerhetsfokus - Kvalitetssäkrad utveckling & Tillgänglighet - Projektbaserad produktutveckling - Avancerad fullstackutveckling, test & devops- LIA",
+    year: "2025-2027",
+  },
+  {
+    name: "Värmdö gymnasium- NaturNatur Journalistik och Forskning",
+    description: "",
+    year: "2017-2020",
+  },
+];
+
+export const work = [
+  {
+    name: "Riddermark bil - Inköpare, Säljare, Administratör",
+    description:
+      "Köpte och sålde personbilar till både privatpersoner och företag. Ansvarade även för administrativa uppgifter som kontraktshantering och betalningar.",
+    year: "2024-2025",
+  },
+  {
+    name: "Vårdcentralen Hökarängen - Vårdbiträde, Receptionist",
+    description:
+      "Skötte administrativt arbete, post, fakturor, bokningar, telefon och registrering av patienter. Utförde hembesök som del av hemsjukvården.",
+    year: "2018-2024",
+  },
+];
+
+export const projects = [
+  {
+    name: "Spa app",
+    description:
+      "En spa-applikation med integrerad webshop, bokningssystem och varukorg. Byggd för att ge användare en smidig upplevelse från att boka behandlingar till att köpa produkter online.",
+    github: "https://github.com/siensky/glowspa",
+    live: "https://glowspa.vercel.app/",
+  },
+  {
+    name: "Bibliotek backend",
+    description:
+      "Byggde ett backend-API för ett bibliotekssystem med Bun och PostgreSQL, med full CRUD-funktionalitet för böcker och författare. Implementerade autentisering med Auth0, rollbaserad åtkomst och ett lånesystem för att låna och lämna tillbaka böcker.",
+    github: "https://github.com/siensky/library",
+    live: "https://library-production-911d.up.railway.app/",
+  },
+  {
+    name: "Bibliotek i php",
+    description:
+      "Ett övningsprojekt i PHP där jag bygger motsvarande endpoints till ett bibliotekssystem. Fokus ligger på att hantera data från en befintlig databas, inklusive att lista böcker och författare samt implementera funktionalitet för boklån.",
+    github: "https://github.com/yourusername/weather-app",
+    live: "https://library-php-1.onrender.com",
+  },
+  {
+    name: "Vinkatalog vanilla JavaScript",
+    description:
+      "Ett responsivt webbprojekt byggt med HTML, CSS och vanilla JavaScript som en del av en bootcamp på Chas Academy. Fokus låg på att fördjupa förståelsen för grunderna i frontendutveckling utan ramverk, med särskild hänsyn till tillgänglighet enligt WCAG och användarvänlig design. Webbplatsen innehåller dynamiskt innehåll hämtat från ett API samt en mobilanpassad navigationsmeny med hamburgerfunktion.",
+    github: "https://https://github.com/siensky/examinationsuppgift",
+    live: "https://wines-bice.vercel.app/index.html",
+  },
+];
